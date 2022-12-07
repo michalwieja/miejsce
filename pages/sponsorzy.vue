f2f1cab6aa562699cd1b644c5ec526f9af7618eb
Michal Wieja <m.wieja@qarbon.it>
2022-12-07 21:44:47 +0100

    check sponsorzy bug 2

--- pages/sponsorzy.vue
+++ pages/sponsorzy.vue
@@ -2,17 +2,19 @@
   <div class="site">
     <div class="container">
       <Title title="Sponsorzy" />
-      <!--      <Swiper />-->
+      <Swiper />
     </div>
   </div>
 </template>
 
 <script>
 import Title from '../components/Title.vue'
+import Swiper from '../components/Swiper.vue'
 
 export default {
   name: 'Sponsorzy',
   components: {
+    Swiper,
     Title
 
   }

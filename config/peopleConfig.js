fbaf87db9230cb06bdf9323003e678089af5288f
Michal Wieja <m.wieja@qarbon.it>
2022-12-14 17:03:11 +0100

    people init

--- config/peopleConfig.js
+++ config/peopleConfig.js
@@ -1,19 +1,19 @@
 export default [
   {
     name: 'Monika Szczepanik',
-    img: 'monika.jpg',
+    img: 'monika.png',
     short: 'Inicjatorka i założycielka Miejsca. Edukatorka, tutorka, trenerka Porozumienia bez Przemocy i Mindfulness. Autorka książki Jak zrozumieć się w rodzinie (Kraków 2020).',
     long: '<p>Inicjatorka i założycielka Miejsca. Edukatorka, tutorka, trenerka Porozumienia bez Przemocy i Mindfulness. Autorka książki Jak zrozumieć się w rodzinie. Organizatorka trzech Konferencji Empatii i wielu rodzinnych obozów w duchu Porozumienia bez Przemocy. Kobieta wspierająca inne kobiety. Zwana przez nie Zwoływaczką.</p><p>Wieloletnia nauczycielka, dyrektorka, trenerka Odysei Umysłu. Jeszcze jako nauczycielka w systemowej szkole stosowała niesystemowe rozwiązania, np. siadając z młodymi w kręgu.</p><p>Wspiera Młodych w byciu w drodze do siebie i do drugiego człowieka. W swojej pracy korzysta z narzędzi, które sama wypróbowała i o których sensie jest głęboko przekonana. Czerpie inspiracje z wielu źródeł, m.in z prac Marii Montessori, Miki Kashtan, Kena Robinsona, Marka Kaczmarzyka, Yaccovy Hechta, Andre Sterna.</p>'
   },
   {
     name: 'Anna Żymełka',
-    img: 'anna.jpg',
+    img: 'anna.png',
     short: 'Tutorka w Miejscu. Z zawodu muzyk – pianistka. Lubi być z ludźmi. Słuchać, by usłyszeć to, co jest ważne. Towarzyszenie młodym ludziom sprawiło, że zaczęła kroczyć nowymi ścieżkami. Odkryła, że relacja jest fundamentem dobrej edukacji.',
     long: '<p>Tutorka. Z zawodu muzyk – pianistka. Lubi być z ludźmi. Słuchać, by usłyszeć to, co jest ważne. Towarzyszenie młodym ludziom sprawiło, że zaczęła kroczyć nowymi ścieżkami. Odkryła, że relacja jest fundamentem dobrej edukacji. Według niej w całym procesie nauczania warto poświęcić czas na dialog i bycie z drugim człowiekiem. Inspiruje się Porozumieniem bez Przemocy. Zgłębia tajniki komunikacji uczestnicząc w licznych warsztatach. Nieustannie poszukuje na edukacyjnej ścieżce. Czerpie inspiracje m. in. z książek  Daniela Siegela czy Andre Sterna oraz Kena Robinsona, bliskie są jej jego słowa “jeśli w jakikolwiek sposób jesteś zaangażowany w edukację, możesz być częścią zmiany”.</p>'
   },
   {
     name: 'Agnieszka Kaczorek',
-    img: 'agnieszka.jpg',
+    img: 'agnieszka.png',
     short: 'Przyrodniczka, ekolożka, edukatorka, trenerką Odysei Umysłu. Las, łąka, pole to moje naturalne środowisko, w którym za pomocą lupy, mikroskopu, aplikacji obserwuje cudowny świat mniejszych i większych istot.',
     long: '<p>Przyrodniczka, ekolożka, edukatorka, trenerka Odysei Umysłu.\n' +
       'Las, łąka, pole to jej naturalne środowisko, w którym za pomocą lupy, mikroskopu, aplikacji obserwuje cudowny świat mniejszych i większych istot.\n' +
@@ -21,13 +21,13 @@ export default [
   },
   {
     name: 'Joanna Gardynik',
-    img: 'asia-g.jpg',
+    img: 'asia-g.png',
     short: 'Psycholożka, trenerka i superwizorka nauczycieli. Lubi przyrodę i rośliny w domu. Jej zadaniem w edukacji i świecie jest przyjmowanie ludzi, takimi, jakimi są. Po prostu.',
     long: '<p>Psycholożka, trenerka i superwizorka nauczycieli. Lubi przyrodę i rośliny w domu. Jej zadaniem w edukacji i świecie jest przyjmowanie ludzi, takimi, jakimi są. Po prostu. To chcę właśnie robić w Miejscu.</p>'
   },
   {
     name: 'Joanna Zabłocka – Skorek',
-    img: 'asia-z-s.jpg',
+    img: 'asia-z-s.png',
     short: 'Doktor nauk o sztuce, filmoznawczyni, kulturoznawczyni, edukatorka. Certyfikowana trenerka w zakresie edukacji medialnej i społecznej. Dyrektorka Interdyscyplinarnego Programu Edukacji Medialnej i Społecznej „KinoSzkoła”, Prezeska Fundacji „KinoSzkoła”, autorka licznych projektów, wystąpień i publikacji z zakresu edukacji kulturalnej.',
     long: '<p>Doktor nauk o sztuce, filmoznawczyni, kulturoznawczyni, edukatorka. Certyfikowana trenerka w zakresie edukacji medialnej i społecznej. Dyrektorka Interdyscyplinarnego Programu Edukacji Medialnej i Społecznej „KinoSzkoła”, Prezeska Fundacji „KinoSzkoła”, autorka licznych projektów, wystąpień i publikacji z zakresu edukacji kulturalnej.\n' +
       'Przez kilka lat pracowała jako nauczycielka wiedzy o kulturze w śląskich szkołach ponadgimnazjalnych.\n' +
@@ -41,7 +41,7 @@ export default [
   },
   {
     name: 'Pan Tomasz',
-    img: 'tomek.jpg',
+    img: 'tomek.png',
     short: 'Astrobiolog. Edukator. Ambasador Edukacji Kosmicznej. Analogowy Astronauta. Trener Nauki i Trener Rozwoju Osobistego. Studiował biologię i wyspecjalizował się w cytogenetyce roślin, popełniając doktorat we współpracy z kapustą, rzepą i rzepakiem.',
     long: '<p>Astrobiolog. Edukator. Ambasador Edukacji Kosmicznej. Analogowy Astronauta. Trener Nauki i Trener Rozwoju Osobistego. Studiował biologię i wyspecjalizował się w cytogenetyce roślin, popełniając doktorat we współpracy z kapustą, rzepą i rzepakiem. Dziś testuje chwasty w habitacie marsjańskim, z misją zastosowania ich w diecie przyszłych astronautów. Ukończył Szkołę Trenerów Meritum w Katowicach. W swojej działalności łączy biologię i astronomię, wspierając młodych w odkrywaniu ich potencjału. Jego osobistym miejscem mocy jest Ciemne Niebo.</p><p>W Miejscu poprowadzi \'Doświadczalnik’ – warsztaty naukowe w cyklu Kolba w obszarze nauk przyrodniczych i \'Rocket Science’ – warsztaty z prowadzenia doświadczeń, dziwienia się, dowodzenia i testowania hipotez oraz pisania prac naukowych. </p>'
   },
@@ -54,7 +54,7 @@ export default [
   },
   {
     name: 'Daniel Wierzbicki',
-    img: 'daniel.jpg',
+    img: 'daniel.png',
     short: 'Instruktor sportowy, tutor, edukator i pasjonat tego wszystkiego, co jest związane z naturą. Lubi szeroko rozumiany outdoor, dlatego buduje szałasy…',
     long: '<p>Instruktor sportowy, tutor, edukator i pasjonat tego wszystkiego, co jest związane z naturą. Lubi szeroko rozumiany outdoor, dlatego buduje szałasy w lesie, przygotowuje kuchnie polowe, gotuje herbatę z liści malin, odpoczywa w hamaku zawieszonym tak, bym nad sobą widział korony drzew. Organizuje wyprawy kajakowe i SUPowe dla dorosłych i młodzieży. Jeździ z nimi na nartach i wspina się na skiturach.</p><p>Młodym w Miejscu będzie towarzyszył w projekcie „Kilka kroków dalej”</p>'
   }
--- sections/ludzie.vue
+++ sections/ludzie.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="site container fh">
+  <div class="site container people">
     <Title title="Ludzie Miejsca" />
-    <div v-for="person in peopleConfig" :key="person.name" class="test">
-      {{ person.name }}
+    <div class="people__cards">
+      <div v-for="person in peopleConfig" :key="person.name" class="people__card">
+        <div>
+          <img :alt="person.name" :src="`/ludzie/${person.img}`">
+        </div>
+        <div class="people__name">
+          {{ person.name }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,7 +31,44 @@ export default {
 
 }
 </script>
-<style>
+<style lang="scss">
+
+.people {
+  &__cards {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 10px;
+
+    @media (max-width: 1200px) {
+      grid-template-columns: 1fr 1fr;
+
+    }
+    @media (max-width: 600px) {
+      grid-template-columns: 1fr;
+
+    }
+  }
+
+  &__card {
+    width: 100%;
+    text-align: center;
+  }
+
+  &__name {
+    font-size: 22px;
+    font-weight: bold;
+    font-family: 'league-spartan', sans-serif;
+
+  }
+
+  img {
+    width: 50%;
+    @media (max-width: 600px) {
+      width: 100%;
+    }
+  }
+}
+
 .test {
   padding-bottom: 2rem
 }

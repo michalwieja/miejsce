a0b0e2a38cd011b624f2508ec0de492bf0720fad
mwieja <mwieja@yahoo.com>
2026-05-07 12:23:11 +0200

    feat: restructure Hero component layout and add CustomButton for contact

--- pages/Hero.vue
+++ pages/Hero.vue
@@ -3,30 +3,38 @@
     <div class="hero__img" />
     <div class="container">
       <Socials />
-      <div class="hero__title desktop-only">
-        <div class="hero__skew-1">
-          Autorskie Liceum
-        </div>
-        <br>
-        <div class="hero__skew-2">
-          w&nbsp;Katowicach
-        </div>
-      </div>
-      <div class="hero__title mobile-only">
-        <div>
+      <div>
+        <div class="hero__title desktop-only">
           <div class="hero__skew-1">
-            Autorskie
+            Autorskie Liceum
           </div>
-        </div>
-        <div>
+          <br>
           <div class="hero__skew-2">
-            Liceum
-          </div>
-        </div>
-        <div>
-          <div class="hero__skew-1">
             w&nbsp;Katowicach
           </div>
+        </div>
+        <div class="hero__title mobile-only">
+          <div>
+            <div class="hero__skew-1">
+              Autorskie
+            </div>
+          </div>
+          <div>
+            <div class="hero__skew-2">
+              Liceum
+            </div>
+          </div>
+          <div>
+            <div class="hero__skew-1">
+              w&nbsp;Katowicach
+            </div>
+          </div>
+        </div>
+        <div class="hero__cta">
+          <CustomButton
+            label="Skontaktuj się z nami"
+            :handle-click="scrollToContact"
+          />
         </div>
       </div>
     </div>
@@ -37,15 +45,33 @@
 </template>
 
 <script>
-
 import Socials from '../components/Socials.vue'
 import ArrowDown from '../components/icons/ArrowDown.vue'
+import CustomButton from '../components/CustomButton.vue'
 
 export default {
   name: 'Hero',
   components: {
     ArrowDown,
-    Socials
+    Socials,
+    CustomButton
+  },
+  methods: {
+    scrollToContact () {
+      const kontaktSection = document.getElementById('kontakt')
+      if (kontaktSection) {
+        kontaktSection.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/variables.scss';
+.hero__cta {
+  margin-top: 3rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
